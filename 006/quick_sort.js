27aab6c1b2b70c000f653109877805d432b52c19
zhaoguowei <529670074@qq.com>
2018-06-11 12:00:46 +0800

    快速排序重新理解

--- 006/quick_sort.js
+++ 006/quick_sort.js
@@ -1,54 +1,32 @@
-// function quick_sort(s,l,r){
-// 	if (l < r)  
-//     {  
-//         var i = l, j = r, x = s[l];  
-//         while (i < j)  
-//         {  
-//             while(i < j && s[j] >= x) // 从右向左找第一个小于x的数  
-//                 j--;    
-//             if(i < j){
-
-//                 s[i++] = s[j];  //s[i]=s[j];i++
-//                 console.log(" "+s);
-//             }
-//              while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数  
-//                 i++;    
-//             if(i < j)   
-//                 s[j--] = s[i];  //s[j] = s[i];j--
-//                 console.log(s+"");
-//         }  
-//         s[i] = x;  
-//         quick_sort(s, l, i - 1); // 递归调用   
-//         quick_sort(s, i + 1, r);  
-//     }  
-// };
-
 /**
 题目：快速排序算法
 思路：两个哨兵，i,j,j从右边找不基数小的，i从左边找比基数大的，然后交换两个目标元素的位置，指导i=j,然后交换i和基数的位置，递归处理。
 **/
 function quick_sort(arr,from,to){
-	var i = from;
-	var j = to;
-	var key = arr[from];
-	if(from >= to){
+	var i = from; //哨兵i
+	var j = to; //哨兵j
+	var key = arr[from]; //标准值
+	if(from >= to){ //如果数组只有一个元素
 	   return;
 	}
 	while(i < j){
-		while(arr[j] >= key && i < j){
+		while(arr[j] > key && i < j){ //从右边向左找第一个比key小的数，找到或者两个哨兵相碰，跳出循环
 			j--;
 		}
-		while(arr[i] <=key && i < j){
+		while(arr[i] <= key && i < j){  //从左边向右找第一个比key大的数，找到或者两个哨兵相碰，跳出循环,这里的=号保证key右边的元素都是大于等于key的
 			i++;
 		}
-		if(i < j){
+		/**
+		  代码执行道这里，1、两个循环到找到了目标值。2、j哨兵找到了目标值。3、两个哨兵都没找到(key是当前数组最小值)
+		**/
+		if(i < j){ //交换两个元素的位置
 			var temp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp;
 
 		}
 	}
-	arr[from] = arr[i]
+	arr[from] = arr[i] //
 	arr[i] = key;
     quick_sort(arr,from,i-1);
 	quick_sort(arr,i+1,to);
